112d441d4decc1c58a94676d8be0ce8da1b088f9
optimum02 <mawou.aa@e-gbavou.com>
2024-10-30 06:25:08 +0100

    refactor tmdb movie api datasource using http call utile function

--- lib/data/datasource/moviedb_api.dart
+++ lib/data/datasource/moviedb_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:da_movie_quiz/core/custom_http_client.dart';
 import 'package:da_movie_quiz/core/failures.dart';
+import 'package:da_movie_quiz/core/utils_function.dart';
 
 import '../dtos/dtos.dart';
 
@@ -10,41 +11,47 @@ final class MovieDBApi {
   const MovieDBApi(this.httpClient);
 
   Future<MovieActorDTO> getRandomActor() async {
-    final response =
-        await httpClient.get('/person/popular?language=en-US&page=1');
+    return await apiCallWithErrorHandling(() async {
+      final response =
+          await httpClient.get('/person/popular?language=en-US&page=1');
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final randomActor = ((data['results'] as List)..shuffle()).first;
-      return MovieActorDTO.fromJson(randomActor);
-    } else {
-      throw ServerFailure();
-    }
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final randomActor = ((data['results'] as List)..shuffle()).first;
+        return MovieActorDTO.fromJson(randomActor);
+      } else {
+        throw ServerFailure();
+      }
+    });
   }
 
   Future<MovieDTO> getRandomMovie() async {
-    final response =
-        await httpClient.get('/movie/popular?language=en-US&page=1');
+    return await apiCallWithErrorHandling(() async {
+      final response =
+          await httpClient.get('/movie/popular?language=en-US&page=1');
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final randomMovie = (data['results']..shuffle()).first;
-      return MovieDTO.fromJson(randomMovie);
-    } else {
-      throw ServerFailure();
-    }
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final randomMovie = (data['results']..shuffle()).first;
+        return MovieDTO.fromJson(randomMovie);
+      } else {
+        throw ServerFailure();
+      }
+    });
   }
 
   Future<MovieDTO> getRandomMovieWithActor(int actorId) async {
-    final response = await httpClient.get('/person/$actorId/movie_credits');
+    return await apiCallWithErrorHandling(() async {
+      final response = await httpClient.get('/person/$actorId/movie_credits');
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final randomMovie = (data['cast']..shuffle()).first;
-      return MovieDTO.fromJson(randomMovie);
-    } else {
-      throw ServerFailure();
-    }
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final randomMovie = (data['cast']..shuffle()).first;
+        return MovieDTO.fromJson(randomMovie);
+      } else {
+        throw ServerFailure();
+      }
+    });
   }
 
   Future<MovieDTO> getRandomMovieExcludingActor(int actorId) async {
@@ -57,14 +64,16 @@ final class MovieDBApi {
   }
 
   Future<bool> checkActorInMovie(int actorId, int movieId) async {
-    final response = await httpClient.get('/movie/$movieId/credits');
+    return await apiCallWithErrorHandling(() async {
+      final response = await httpClient.get('/movie/$movieId/credits');
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final cast = data['cast'] as List;
-      return cast.any((member) => member['id'] == actorId);
-    } else {
-      throw ServerFailure();
-    }
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final cast = data['cast'] as List;
+        return cast.any((member) => member['id'] == actorId);
+      } else {
+        throw ServerFailure();
+      }
+    });
   }
 }
