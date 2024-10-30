6a350982bdf9ec97bf73bf2eb3155c0e64f7d0d1
optimum02 <mawou.aa@e-gbavou.com>
2024-10-30 17:05:51 +0100

    add toMessage method to Failure class

--- lib/core/failures.dart
+++ lib/core/failures.dart
@@ -1,11 +1,28 @@
-abstract interface class Failure {}
+abstract interface class Failure {
+  String toMessage();
+}
 
-class ServerFailure extends Failure {}
+class ServerFailure extends Failure {
+  @override
+  String toMessage() {
+    return "Server error. Please try again later";
+  }
+}
 
-class NoInternetConnexion extends Failure {}
+class NoInternetConnexion extends Failure {
+  @override
+  String toMessage() {
+    return "No internet connexion. Check and retry";
+  }
+}
 
 class UnexpectedFailure extends Failure {
   final dynamic error;
 
   UnexpectedFailure(this.error);
+
+  @override
+  String toMessage() {
+    return "An unexpected error occured ! Please try later";
+  }
 }
